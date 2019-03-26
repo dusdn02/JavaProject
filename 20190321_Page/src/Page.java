bf8963a4f5f724dd95d20369099d2427ccc83767
dusdn02 <lyw9114@naver.com>
2019-03-26 19:32:10 +0900

    add Page Project

---
+++ 20190321_Page/src/Page.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Page {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+
+			System.out.println("몇개의 게시물이 있나요? (정수로 쓰세요)");
+			int result = sc.nextInt();
+			float page = (float) result / 7;
+
+			if (page - (int) page > 0)
+				page += 1;
+
+			System.out.println((int) page + "페이지");
+
+			System.out.println("종료하려면 exit를 입력해주세요");
+
+			String choice = sc.next();
+
+			if ("exit".equals(choice)) {
+				System.out.println("종료합니다.");
+				System.exit(0);
+			}
+		}
+
+//		Scanner sc = new Scanner(System.in);
+//
+//		int nPerPage = 7;
+//
+//		while (true) {
+//
+//			System.out.println("몇 개의 게시물을 표시할까요?(정수로 쓰세요.)  종료.(0)");
+//			int result = sc.nextInt();
+//			
+//			if(result==0) {
+//				System.out.println("끝");
+//				break;
+//			}
+//			
+//			int 결과 = result / nPerPage;
+//			if (result % nPerPage != 0)
+//				결과 = result / nPerPage + 1;
+//
+//			System.out.println(결과 + "페이지가 필요합니다.");
+//
+//		}
+	}
+}
