110496aacbeaa97effb1546c3c18571d594e63c4
dusdn02 <lyw9114@naver.com>
2019-03-28 19:00:00 +0900

    add UpDown Project

--- 20190326_Echo/src/Echo.java
+++ 20190326_Echo/src/Echo.java
@@ -7,16 +7,17 @@ public class Echo {
 
 		while (true) {
 
-			System.out.println("입력하세요.");
+			System.out.println("입력>>");
 			String inputString = sc.nextLine();
 
 			if (inputString.equals("exit"))
 				break;
 
 			System.out.println("출력>>" + inputString);
-		}
+		}//while
+		
 		System.out.println("종료합니다.");
 
-	}
+	}//main
 
 }
---
+++ 20190326_UpDown/src/UpDown.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class UpDown {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		Random random = new Random();
+
+		int rNumber = random.nextInt(100);
+		int count=0;
+
+//		System.out.println(rNumber);
+
+		while (true) {
+
+			System.out.println("1~100의 숫자 하나를 입력하세요.");
+			int you = sc.nextInt();
+			count++;
+
+			if (you == rNumber) {
+				System.out.println("정답!!");
+				break;
+			} else if (you < rNumber)
+				System.out.println("UP");
+			else
+				System.out.println("DOWN");
+
+		}//while
+		
+		System.out.println(count + "번만에 맞추셨습니다!!");
+
+	}
+
+}
