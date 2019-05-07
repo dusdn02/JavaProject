e7a3bb9588e91961278c64f4ee4296efca080cff
dusdn02 <lyw9114@naver.com>
2019-05-07 19:58:24 +0900

    add CheckBox

---
+++ 20190430_GUI/src/GUITest3_JCheckBox.java
@@ -0,0 +1,30 @@
+import java.awt.Dimension;
+
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class GUITest3_JCheckBox {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("창이름");
+		frame.setLocation(1200, 700);
+		frame.setPreferredSize(new Dimension(300,200));
+		
+		JPanel panel = new JPanel();//컨퍼런트가 여러개 나올 수 있도록 함
+		JCheckBox cbChicken = new JCheckBox("치킨");
+		JCheckBox cbPizza = new JCheckBox("피자");
+		
+		panel.add(cbChicken);
+		panel.add(cbPizza);
+		frame.add(panel);
+		
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
