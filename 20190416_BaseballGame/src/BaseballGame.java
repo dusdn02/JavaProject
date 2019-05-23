daae6401d60ed7010a6346583c6fee01d5f377a2
dusdn02 <s2018s14@e-mirim.hs.kr>
2019-05-23 20:12:53 +0900

    temp

--- 20190416_BaseballGame/src/BaseballGame.java
+++ 20190416_BaseballGame/src/BaseballGame.java
@@ -8,7 +8,7 @@ public class BaseballGame {
 		int ball = 0, strike = 0;
 		Scanner sc = new Scanner(System.in);
 		Random random = new Random();
-
+//temp
 		int ran[] = new int[SIZE];
 		int num[] = new int[SIZE];
 
