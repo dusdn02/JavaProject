8ea448d80d8b6888bdc6b5e0d7e2a6802812c6d5
dusdn02 <lyw9114@naver.com>
2019-07-25 15:49:48 +0900

    Binary copy

---
+++ SchoolVacation/src/_0725/TextCopy.java
@@ -0,0 +1,36 @@
+package _0725;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TextCopy {
+	public static void main(String[] args) {
+		File src = new File("c:\\windows\\system.ini");
+		File dst = new File("C:\\javaproject\\SchoolVacation\\src\\_0725\\system.txt");
+		FileReader fr = null;
+		FileWriter fw = null;
+		BufferedReader in =null;
+		BufferedWriter out = null;
+		int c;
+		try {
+			fr= new FileReader(src);
+			fw = new FileWriter(dst);
+			in= new BufferedReader(fr);
+			out = new BufferedWriter(fw);
+			while((c= in.read())!=-1) {
+				out.write((char)c);
+			}
+			in.close();
+			out.close();
+			fr.close();
+			fw.close();
+		}catch(IOException e) {
+			System.out.println("입출력 오류");
+		}
+	}
+
+}
