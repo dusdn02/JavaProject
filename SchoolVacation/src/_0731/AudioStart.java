aef31fb8c77d5d0c8316fe2e8ef5a3f5f28b0ee3
dusdn02 <lyw9114@naver.com>
2019-07-31 14:35:59 +0900

    audio를 실행

--- SchoolVacation/src/_0731/AudioStart.java
+++ SchoolVacation/src/_0731/AudioStart.java
@@ -5,45 +5,87 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
 import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 
-public class AudioStart extends JFrame{
+public class AudioStart extends JFrame {
 	private Clip clip = null;
 	private AudioInputStream audioStream = null;
 	private JLabel msgLabel = new JLabel("오디오 파일");
 	private JButton sbtn = new JButton("연주");
 	private JButton ebtn = new JButton("종료");
-	
+
 	public AudioStart() {
 		super("오디오 파일을 연주/종료 제어");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container c = getContentPane();
 		c.setLayout(new FlowLayout());
-		msgLabel.setFont(new Font("Gothic",Font.PLAIN,15));
+		msgLabel.setFont(new Font("Gothic", Font.PLAIN, 15));
 		msgLabel.setHorizontalAlignment(JLabel.CENTER);
 		sbtn.setHorizontalAlignment(JButton.CENTER);
 		ebtn.setHorizontalAlignment(JButton.RIGHT);
 		c.add(msgLabel);
 		c.add(sbtn);
 		c.add(ebtn);
-		
-		setSize(450,150);
+
+		setSize(450, 150);
 		setVisible(true);
-		
+
 		sbtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(clip !=null && clip.isActive())
+				if (clip != null && clip.isActive())
 					clip.close();
-				
+
+				playAudio("src/swing/hiphop.wav");
+				msgLabel.setText("hippop.wav" + " 를 연주하고 있습니다.");
+
 			}
 		});
+
+		ebtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (clip != null && clip.isActive())
+					clip.close();
+
+				msgLabel.setText("연주를 종료합니다.");
+
+				try {
+					audioStream.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+
+		});
 	}
+
+	private void playAudio(String pathName) {
+		try {
+			File audioFile = new File(pathName);
+			audioStream = AudioSystem.getAudioInputStream(audioFile);
+
+			clip = AudioSystem.getClip();
+			clip.open(audioStream);
+			clip.start();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
 		new AudioStart();
 	}
